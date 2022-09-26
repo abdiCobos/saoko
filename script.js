0a7df9d26879fe34b07206cf2d848aba0e3df990
Abdi Cobos <abdi0804@gmail.com>
2022-09-26 09:51:55 -0600

    feat: uso de script externo

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Saoko papi, saoko')
+document.getElementById("generation").style.color = "red";
+function colorTitulo(){
+  console.log('Pulsó el botón de generation')
+  document.getElementById("generation").style.color = "blue";
+}
